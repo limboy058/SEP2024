dd1ff18a578c8d7472ee9f1efec6898d5388f946
limboy058 <2324899904@qq.com>
2024-07-10 11:14:42 +0800

    更新了request接口

--- fe/minipro/pages/fun1/fun1.js
+++ fe/minipro/pages/fun1/fun1.js
@@ -4,11 +4,13 @@ Page({
    */
   data: {
     aiResponse: '提交后，此处将显示AI回复',
-    voiceInput: ''
+    voiceInput: '',
+    loginCode:''
   },
 
   // Function to handle "拍照" button click
   button1: function () {
+    
     const that = this;
     wx.chooseMedia({
       count: 1, //只能选择一张
@@ -17,11 +19,12 @@ Page({
         that.setData({
           aiResponse: "思考中......请等候约5秒"
         });
+        console.log(this.data.loginCode)
         wx.request({
           url: 'http://10.9.176.40:8123',//? unknown
           method: 'POST',
           data: {
-            userid: 'yourUserIdHere',
+            logincode: this.data.loginCode,
             newtalk: true, // 或者 false，根据实际情况设置
             kind: '1',
             picture: 'base64编码的图片数据', // 如果是base64编码的图片数据，可以直接传字符串
@@ -40,7 +43,7 @@ Page({
           fail: (error) => {
             console.error('请求失败', error); // 请求失败时的处理逻辑
             that.setData({
-              aiResponse: "网络连接异常"
+              aiResponse: "网络连接异常"+this.data.loginCode
             });
           }
         });
@@ -75,7 +78,17 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {},
+  onReady() {
+    wx.login({
+      //成功放回
+      success:(res)=>{
+        console.log(res);
+        this.setData({
+          loginCode: res.code
+        });
+      }
+    })
+  },
 
   /**
    * 生命周期函数--监听页面显示
